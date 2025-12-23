d21826e2d6f46b7dff41ab9a714c8e149a4b66fa
diegoworks92 <diegoreyes@diegoworks.com>
2025-12-23 21:16:56 +0100

    feat: install TypeScript in the frontend folder on the develop branch

--- proyecto-final/frontend/src/main.jsx
+++ proyecto-final/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./App.jsx"
+import App from "./App.tsx"
 import { ThemeProvider } from "./contexts/ThemeContext.jsx"
 import { BrowserRouter } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
